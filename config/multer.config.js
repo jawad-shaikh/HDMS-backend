5966e65a2dfeb7c8187977dc0d2366b8b46f0177
jawad shaikh <shaikhjawad314@gmail.com>
2024-01-01 19:39:13 +0500

    done

--- config/multer.config.js
+++ config/multer.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const multer = require('multer');
 
 const staffDocumentsStorage = multer.diskStorage({
-  destination: 'uploads/documents',
+  destination: 'uploads/staff-documents',
   filename: (req, file, cb) => {
     const fileExt = path.extname(file.originalname);
     const finalFilename = `${file.fieldname}-${Date.now()}${fileExt}`;
